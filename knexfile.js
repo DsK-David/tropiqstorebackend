01dc3b8a14519f201f9968a451efa647a0615b84
David silva <fbi.gov2015@gmail.com>
2025-07-29 16:36:04 -0100

    melhorando a api e as rotas

--- knexfile.js
+++ knexfile.js
@@ -1,31 +1,12 @@
-// module.exports = {
-//   development: {
-//     client: 'mysql2',
-//     connection: {
-//       host: 'mysql-blogdavid.alwaysdata.net',
-//       port: 3306,
-//       user: 'blogdavid',
-//       password: 'dskdavid2513',
-//       database: 'blogdavid_tropiqstore'
-//     },
-//     pool: {
-//       min: 2,
-//       max: 10
-//     },
-//     migrations: {
-//       tableName: 'knex_migrations'
-//     }
-//   }
-// };
 module.exports = {
   development: {
     client: 'mysql2',
     connection: {
-      host: 'localhost',
+      host: 'mysql-blogdavid.alwaysdata.net',
       port: 3306,
-      user: 'root',
+      user: 'blogdavid',
       password: 'dskdavid2513',
-      database: 'tropiq_store'
+      database: 'blogdavid_tropiqstore'
     },
     pool: {
       min: 2,
@@ -36,3 +17,22 @@ module.exports = {
     }
   }
 };
+// module.exports = {
+//   development: {
+//     client: 'mysql2',
+//     connection: {
+//       host: 'localhost',
+//       port: 3306,
+//       user: 'root',
+//       password: 'dskdavid2513',
+//       database: 'tropiq_store'
+//     },
+//     pool: {
+//       min: 2,
+//       max: 10
+//     },
+//     migrations: {
+//       tableName: 'knex_migrations'
+//     }
+//   }
+// };
